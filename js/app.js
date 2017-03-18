c05012935c4cc9ae40505174ce20ae6bfa4be99e
AdamBigos <AdamBigos@users.noreply.github.com>
2017-03-18 10:16:32 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,34 +1,43 @@
 var main = function() {
     $('.close_ikon').click(function() {
-    $('.menu').hide(); $('.top_wraper').show();
-   });
+        $('.menu').hide();
+        $('.top_wraper').show();
+    });
     
     $('.top_wraper').click(function() {
-    $('.menu').show(); $('.top_wraper').hide();
-   });
+        $('.menu').show();
+        $('.top_wraper').hide();
+    });
     
-  $('.article_title').click(function() {
-  $('.article_title').removeClass('current');
-  $('.description_row').hide();
+    $('.article').click(function() {
+        $('.article').removeClass('current');
+        $('p').hide();
 
-    $(this).addClass('current');
-    $(this).next('.description_row').show();
-  });
+        $(this).addClass('current');
+        $(this).children().show();
+    });
 
-  $(document).keypress(function(event) {
-    if(event.which === 111) {
-      $('.description_row').hide();
-      $('.current').children('.description_row').show();
-    }
+    $(document).keypress(function(event) {
+        if(event.which === 111) {
+            $('p').hide();
+            $('.current').children('p').toggle();
+        }
 
-    else if(event.which === 110) {
-      var currentArticle = $('.current');
-      var nextArticle = currentArticle.next();
+        else if(event.which === 110) {
+            var currentArticle = $('.current');
+            var nextArticle = $('.current').next();
       
-      currentArticle.removeClass('current');
-      nextArticle.addClass('current');
-    }
-  });
+            currentArticle.removeClass('current');
+            nextArticle.addClass('current');
+        }
+        else if(event.which === 112){
+            var currentArticle = $('.current');
+            var previueArticle = $('.current').prev();
+          
+            currentArticle.removeClass('current');
+            previueArticle.addClass('current');
+        }
+    });
 }
 
 $(document).ready(main);
